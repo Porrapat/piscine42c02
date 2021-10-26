46214956136e4ebc536b487a6ba50c1feeb1ba26
Porrapat Petchdamrongskul <porrapat.cpe20@gmail.com>
2021-10-26 21:28:59 +0700

    Add more

--- test_ex11_ft_putstr_non_printable.c
+++ test_ex11_ft_putstr_non_printable.c
@@ -9,3 +9,16 @@
 /*   Updated: 2021/10/20 23:53:55 by porrapat         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+#include <stdio.h>
+#include <stdbool.h>
+#include <unistd.h>
+
+void	ft_putstr_non_printable(char *str);
+
+int		main(void)
+{
+	char	*string;
+
+	string = "Coucou\ntu vas bien ?";
+	ft_putstr_non_printable(string);
+}
