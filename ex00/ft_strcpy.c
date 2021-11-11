373ab9d8292491b620952a4aac48d9d2a4999c5d
Porrapat Petchdamrongskul <porrapat.cpe20@gmail.com>
2021-11-11 20:10:33 +0700

    Clean FT Strcpy

--- ex00/ft_strcpy.c
+++ ex00/ft_strcpy.c
@@ -12,14 +12,7 @@
 
 char	*ft_strcpy(char *dest, char *src)
 {
-	int	index;
-
-	index = 0;
-	while (src[index] != '\0')
-	{
-		dest[index] = src[index];
-		index++;
-	}
-	dest[index] = '\0';
+	while (*src)
+		*dest++ = *src++;
 	return (dest);
 }
